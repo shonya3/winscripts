d251a841982c013b10a2b431ac50ae7df1be6059
shonya3 <poeshonya3@gmail.com>
2024-06-18 12:11:18 +0300

    rust duplicate

--- commands.ps1
+++ commands.ps1
@@ -7,7 +7,6 @@ winget install --exact --id Microsoft.VisualStudioCode.Insiders
 winget install --exact --id OpenJS.NodeJS --silent
 winget install --exact --id Git.Git 
 winget install --exact --id pnpm.pnpm
-winget install --exact --id Rustlang.Rustup
 winget install --exact --id Google.Chrome --silent
 winget install --exact --id Mozilla.Firefox --silent
 winget install --exact --id VideoLAN.VLC 
